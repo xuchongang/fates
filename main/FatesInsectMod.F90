655d8c33e64a2e407a7d25d3b2469338d67d7e86
Chonggang Xu <xuchongang@gmail.com>
2018-09-25 14:58:20 -0600

    Turned on Yellowstone MPB initialization.

--- main/FatesInsectMod.F90
+++ main/FatesInsectMod.F90
@@ -247,21 +247,21 @@ contains
     
     ! Here's a hack to initialize the model with density of insects appropriate for 
     ! Yellowstone National Park.
-    !if(hlm_current_year == 2001 .and. hlm_current_month == 7 .and. hlm_current_day == 21) then
+    if(hlm_current_year == 2001 .and. hlm_current_month == 7 .and. hlm_current_day == 21) then
         ! The model is initialized with the number of beetles that is consistent with the size of the outbreak in 2001
 	! according to our attack model.
-	!FA = 615.4656_r8
-	!FebInPopn = 615.4656_r8
-    !end if
+	FA = 615.4656_r8
+	FebInPopn = 615.4656_r8
+    end if
     
     ! Here's a hack to initialize the model with density of insects appropriate for 
     ! Rocky Mountain National Park.
-    if(hlm_current_year == 2006 .and. hlm_current_month == 7 .and. hlm_current_day == 21) then
+    !if(hlm_current_year == 2006 .and. hlm_current_month == 7 .and. hlm_current_day == 21) then
         ! The model is initialized with the number of beetles that is consistent with the size of the outbreak in 2006
 	! according to our attack model.
-	FA = 4812.636_r8
-	FebInPopn = 4812.636_r8
-    end if
+	!FA = 4812.636_r8
+	!FebInPopn = 4812.636_r8
+    !end if
     
     ! Here's a hack to initialize the model with density of insects appropriate for 
     ! Glacier National Park.
